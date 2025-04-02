65abc8b2b91874b30ae3c42e38f3b0a9b62270cc
Vitor Lucas Fujita Felício <vlucasff@hotmail.com>
2025-04-02 13:32:58 -0300

    feat: user is logged

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -6,26 +6,34 @@ import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-form',
-  imports: [ FormsModule, CommonModule,FormsModule],
+  imports: [ FormsModule, CommonModule, FormsModule ],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
 export class FormComponent {
-  // Propriedades necessárias
   name: string = "";
   password: string = "";
   resultado: any;
+  isLoginMode: boolean = false; // Modo padrão: registro
 
   private baseUrl = "http://localhost:8080/auth";
 
   constructor(private http: HttpClient) {}
 
+  onAction() {
+    if (this.isLoginMode) {
+      this.loginLifter();
+    } else {
+      this.registerLifter();
+    }
+  }
+
   registerLifter() {
     this.postRegisterLifter().subscribe({
       next: (res) => {
         console.log("Lifter cadastrado com sucesso! ID:", res.id);
-        localStorage.setItem("lifterId", res.id); // Armazena o ID no localStorage
-        this.resultado = "Registered user!";
+        localStorage.setItem("lifterId", res.id);
+        this.resultado = "Registered user! Now try to Login";
       },
       error: (err) => {
         console.error('Erro, já existe um user:', err);
@@ -34,12 +42,40 @@ export class FormComponent {
     });
   }
 
+  loginLifter() {
+    this.postLoginLifter().subscribe({
+      next: (res) => {
+        console.log("Lifter logado com sucesso! Token:", res.id);
+        localStorage.setItem("token", res.id); // Armazena o token
+        this.resultado = "User logged in!";
+      },
+      error: (err) => {
+        console.error('Erro ao logar:', err);
+        this.resultado = 'Login failed. Check your credentials.';
+      }
+    });
+  }
+
   public postRegisterLifter(): Observable<any> {
     return this.http.post<any>(
-      `${this.baseUrl}/register`,{
+      `${this.baseUrl}/register`, {
         name: this.name,
         password: this.password
       }
     );
+  }
+
+  public postLoginLifter(): Observable<any> {
+    return this.http.post<any>(
+      `${this.baseUrl}/login`, {
+        name: this.name,
+        password: this.password
+      }
+    );
+  }
+
+  toggleMode() {
+    this.isLoginMode = !this.isLoginMode;
+    this.resultado = null; // Limpar a mensagem de resultado ao trocar de modo
   }
 }
